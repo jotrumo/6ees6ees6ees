6e3ad31d5d6f1afd2aece01a3dc3008c70902386
jotrumo <jtmontoya26@gmail.com>
2020-05-26 19:22:54 -0700

    HoneyMakerBee Class complete - all tests passing

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
-class HoneyMakerBee {
+class HoneyMakerBee extends Bee {
   // TODO..
+  constructor(age, color, food, job, honeyPot) {
+    super();
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+
+  makeHoney() {
+    this.honeyPot = this.honeyPot + 1;
+  }
+
+  giveHoney() {
+    this.honeyPot = this.honeyPot - 1;
+  }
 };
