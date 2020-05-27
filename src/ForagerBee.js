8cb8fe385364a9f49dd3a581e5be0e14d50ed5e2
jotrumo <jtmontoya26@gmail.com>
2020-05-26 19:25:16 -0700

    ForagerBee Class complete - all tests passing

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
-class ForagerBee {
+class ForagerBee extends HoneyMakerBee{
   // TODO..
+  constructor(age, color, food, job, honeyPot, canFly) {
+    super();
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
+
 };
